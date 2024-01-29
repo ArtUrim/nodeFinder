64523504601a9d5df2056f5bdb042864730253f8
Artur Łoziński <lozinski.artur@gmail.com>
2024-01-29 19:28:38 +0100

    in case of all python, use tomli

--- node/nodeDiscover.py
+++ node/nodeDiscover.py
@@ -4,29 +4,36 @@
 import platform
 import subprocess
 import json
-import requests
 
-import toml
-import logging
-
-import macInfo
-
-with open( 'config.toml','rt') as fh:
-    config = toml.load(fh)
+req_installed = None
+try:
+    import requests
+    req_installed = True
+except ModuleNotFoundError:
+    print( "No requests module installed - print result to result.json file" )
 
 try:
-    ADDRESS = config['server']['ip']
-    PORT    = config['server']['port']
-    NODE_HEADERS = config['http']['header']
-except KeyError as ke:
-    print( "Unknown field {} in config.toml".format(e) )
-    exit(1)
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+import logging
+import re
+
+import argparse
+ 
+noNetifaces = None
+try:
+    import macInfo
+except ModuleNotFoundError:
+    noNetifaces = True
+
 
 def jsonify( strj: str ):
     try:
         js = json.loads(strj)
     except json.JSONDecodeError as jde:
-        logging.error( "Cannot convert to JSON: {}".format(jde.error) )
+        logging.error( "Cannot convert to JSON: {}".format(jde.msg) )
         js = jde.doc
     return js
 
@@ -34,15 +41,25 @@ def jsonifyTable( strj: str ):
     s2 = '[' + strj.strip() + ']'
     return jsonify( s2.replace('\n',',') )
 
-def installedApp( jDict, app, detailQueries = {}, versQuery = '--version' ):
+def installedApp( jDict, app, detailQueries = {}, versQuery = '--version', name=None ):
+    if not name:
+        name = app
     subp = subprocess.run( ["/usr/bin/env", app, versQuery],
                           capture_output=True )
     if subp.returncode == 0:
-        jDict[app] = subp.stdout.decode().strip()
+        verr = subp.stdout.decode().strip()
+        m = re.search( '\d+\.\d+\.\d+', verr )
+        if m:
+            verr = m.group()
+        else:
+            m = re.search( '\d+\.\d+', verr )
+            if m:
+                verr = m.group()
+        jDict[name] = verr
     else:
-        jDict[app] = None
+        jDict[name] = None
     if detailQueries:
-        jDict[app + '_details'] = {}
+        jDict[name + '_details'] = {}
         for dq in detailQueries:
             queries = ["/usr/bin/env", app]
             doFunc = None
@@ -64,11 +81,69 @@ def installedApp( jDict, app, detailQueries = {}, versQuery = '--version' ):
             subp = subprocess.run( queries, capture_output =  True )
             if subp.returncode == 0:
                 if doFunc:
-                    jDict[app + '_details'][dq] = doFunc(subp.stdout.decode())
+                    jDict[name + '_details'][dq] = doFunc(subp.stdout.decode())
                 else:
-                    jDict[app + '_details'][dq] = subp.stdout.decode().strip()
-    
+                    jDict[name + '_details'][dq] = subp.stdout.decode().strip()
+
+def findPyVenv( jDict: dict ):
+    name = "venv"
+    jDict[name] = None
+    res = subprocess.run( [ "/usr/bin/env", "pip3", "show", name ],
+                         capture_output = True )
+    if res.returncode == 0:
+        verr = subp.stdout.decode().strip()
+        m = re.search( '\d+\.\d+\.\d+', verr )
+        if m:
+            verr = m.group()
+        else:
+            m = re.search( '\d+\.\d+', verr )
+            if m:
+                verr = m.group()
+        jDict[name] = verr
+    else:
+        res = subprocess.run( [ "/usr/bin/env", "apt", "list", "python3-" + name ],
+                             capture_output = True )
+        if res.returncode == 0:
+            sl = res.stdout.decode().split('\n')
+            for s in sl:
+                if s.endswith('[installed]'):
+                    m = re.search( '\d+(\.\d+){2,3}', s )
+                    if m:
+                        jDict[name] = m.group()
+
+def inputParse(adr,port):
+    parser = argparse.ArgumentParser()
+    parser.add_argument( "-l", help="send to localhost", action='store_true' )
+    parser.add_argument( "-i", help="send to ip address", type=str )
+    parser.add_argument( "-p", help="send to ip port", type=str )
+    args = parser.parse_args()
+    if args.l and args.i:
+        print( "Either localhost or implicite ip address" )
+        exit(1)
+    if args.l:
+        adr = "127.0.0.1"
+    if args.i:
+        adr = args.i
+    if args.p:
+        port = args.p
+    return (adr,port)
+
 if __name__ == "__main__":
+
+    with open( 'config.toml','rb') as fh:
+        config = tomllib.load(fh)
+
+    try:
+        ADDRESS = config['server']['ip']
+        PORT    = config['server']['port']
+        NODE_URL = config['server']['url']
+        NODE_HEADERS = config['http']['header']
+    except KeyError as ke:
+        print( "Unknown field {} in config.toml".format(e) )
+        exit(1)
+
+    (ADDRESS,PORT)=inputParse(ADDRESS, PORT)
+
     nodeInfo = {}
     nodeInfo["platform"] = platform.machine()
     nodeInfo["node"] = platform.node()
@@ -77,12 +152,22 @@ if __name__ == "__main__":
 
     if 'freedesktop_os_release' in dir(platform):
         nodeInfo["lsb"] = {}
-        for k,v in platform.freedesktop_os_release().items():
-            nodeInfo["lsb"][k] = v
+        try:
+            for k,v in platform.freedesktop_os_release().items():
+                nodeInfo["lsb"][k] = v
+        except FileNotFoundError:
+            logging.warning( "Cannot read lsb_release file" )
     else:
-        logging.warn( "Module platform does not contain freedesktop_os_release" )
+        logging.warn( "TODO: (#3) Module platform does not contain freedesktop_os_release" )
 
     installedApp( nodeInfo, "python3" )
+    nodeInfo["python3_details"] = {}
+    installedApp( nodeInfo["python3_details"], "pip3" )
+    installedApp( nodeInfo["python3_details"], "pipenv" )
+    installedApp( nodeInfo["python3_details"], "poetry" )
+    installedApp( nodeInfo["python3_details"], "pytest" )
+    findPyVenv( nodeInfo['python3_details'] )
+
     installedApp( nodeInfo, "docker", 
                  detailQueries = {
                      "images": { "query": ["images","--format","json","--digests"],
@@ -91,10 +176,20 @@ if __name__ == "__main__":
                                     "func": jsonifyTable }
                      }
                  )
+    installedApp( nodeInfo, "ldd", name="glibc" )
+    installedApp( nodeInfo, "openssl", versQuery="version" )
 
-    nodeInfo["netifaces"] = macInfo.ifaces()
-    nodeInfo["gateways"] = macInfo.gates()
+    if not noNetifaces:
+        nodeInfo["netifaces"] = macInfo.ifaces()
+        nodeInfo["gateways"] = macInfo.gates()
+    else:
+        logging.error( "TODO: (#4) backup for netifaces (route -n and ip a)" )
 
-    requests.post( "http://" + ADDRESS + ":" + PORT,
+    logging.info( f"ADDRESS:{ADDRESS} PORT:{PORT}" )
+if req_installed:
+    requests.post( "http://" + ADDRESS + ":" + PORT + NODE_URL,
                   data = json.dumps( nodeInfo ),
                   headers = NODE_HEADERS )
+else:
+    with open( "result.json", "wt" ) as fh:
+        json.dump( nodeInfo, fh )
